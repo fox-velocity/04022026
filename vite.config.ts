ebed3ea6913235d6c21d00e08788bf3c2c0bf93f
FoxVelocity <foxvelocity@laposte.net>
2026-02-04 18:53:15 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/impot/', // Indispensable pour https://fox-velocity.github.io/impot/
+  base: '/04022026/', // Doit correspondre exactement au nom du dépôt GitHub
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    sourcemap: false,
   }
 });
