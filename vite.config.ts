a4e7d1f558724aca2a4d030703213749e406dc46
FoxVelocity <foxvelocity@laposte.net>
2026-02-04 19:14:31 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/04022026/', // Doit correspondre exactement au nom du dépôt GitHub
+  base: '/04022026/', // Doit correspondre exactement au nom de votre dépôt
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
+    emptyOutDir: true
   }
 });
